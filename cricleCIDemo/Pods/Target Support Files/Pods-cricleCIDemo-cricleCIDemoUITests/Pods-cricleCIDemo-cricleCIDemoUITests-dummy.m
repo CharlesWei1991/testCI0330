5c829fee6edf145e4832c4391565c5793056a9e0
wei.zhx <charles_wei_1991@126.com>
2023-03-30 15:42:37 +0800

    no message

---
+++ cricleCIDemo/Pods/Target Support Files/Pods-cricleCIDemo-cricleCIDemoUITests/Pods-cricleCIDemo-cricleCIDemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cricleCIDemo_cricleCIDemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_cricleCIDemo_cricleCIDemoUITests
+@end
---
+++ cricleCIDemo/Pods/Target Support Files/Pods-cricleCIDemoTests/Pods-cricleCIDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cricleCIDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_cricleCIDemoTests
+@end
