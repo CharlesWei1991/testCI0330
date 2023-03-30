e11a19db1f1f16b69351e0171edcb5793cb914de
wei.zhx <charles_wei_1991@126.com>
2023-03-30 15:59:05 +0800

    no message

--- cricleCIDemo/ViewController.swift
+++ cricleCIDemo/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("go")
+        var a:String?
+        a = nil
+        var b = a!
+        
     }
 
 
